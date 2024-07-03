0f43366a087c0fb96b829e5e8aea5c688e6c23bf
JYC0413 <juyichen0413@foxmail.com>
2024-07-03 17:37:43 +0800

    Upload multiple files and rounds of conversations

--- pdfRender.ts
+++ pdfRender.ts
@@ -7,7 +7,7 @@ import {
     OpenAI,
     OpenAIEmbedding,
     QdrantVectorStore,
-    Settings,
+    Settings, SimpleDirectoryReader,
     storageContextFromDefaults,
     VectorStoreIndex,
 } from "llamaindex";
@@ -20,10 +20,20 @@ async function main() {
     const rl = readline.createInterface({ input, output });
     Settings.llm = new OpenAI({model: process.env.LLAMAEDGE_CHAT_MODEL, temperature: 0});
     Settings.embedModel = new OpenAIEmbedding({model: process.env.LLAMAEDGE_EMBEDDING_MODEL});
-
-    const path = process.env.FILE_PATH;
-
-    const reader = new LlamaParseReader({resultType: "markdown"});
+    const dir = process.env.FILE_DIR;
+    let path = process.env.FILE_PATH;
+    let reader
+    if(dir) {
+        const isDir = await rl.question("Do you want to read from the folder(Y or N)");
+        if(!isDir || isDir.toUpperCase() === "Y") {
+            path = dir
+            reader = new SimpleDirectoryReader();
+        }else {
+            reader = new LlamaParseReader({resultType: "markdown"});
+        }
+    }else {
+        reader = new LlamaParseReader({resultType: "markdown"});
+    }
 
     if(path){
         const documents = await reader.loadData(path)
@@ -36,22 +46,25 @@ async function main() {
         const index = await VectorStoreIndex.fromDocuments(documents, {
             storageContext: ctx,
         });
-        const query = await rl.question("Query: ");
-        const queryEngine = index.asQueryEngine();
-        const {response, sourceNodes} = await queryEngine.query({
-            query
-        });
-
-        process.stdout.write(response);
-        rl.close();
-
-        if (sourceNodes) {
-            sourceNodes.forEach((source: NodeWithScore, index: number) => {
-                console.log(
-                    `\n${index}: Score: ${source.score} - ${source.node.getContent(MetadataMode.NONE).substring(0, 50)}...\n`,
-                );
+        while (true){
+            const query = await rl.question("Query: ");
+            const queryEngine = index.asQueryEngine();
+            const {response, sourceNodes} = await queryEngine.query({
+                query
             });
+
+            process.stdout.write("Answer:"+response);
+
+            if (sourceNodes) {
+                sourceNodes.forEach((source: NodeWithScore, index: number) => {
+                    console.log(
+                        `\n${index}: Score: ${source.score} - ${source.node.getContent(MetadataMode.NONE).substring(0, 50)}...\n`,
+                    );
+                });
+            }
         }
+
+        // rl.close();
     }
 }
 
