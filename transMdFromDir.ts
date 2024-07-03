3d2596f02956d92b9ae5b65dcab2ececcd76919e
JYC0413 <juyichen0413@foxmail.com>
2024-07-03 17:04:45 +0800

    add transMdFromDir

---
+++ transMdFromDir.ts
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+import {promises as fs} from 'fs';
+import {
+    LlamaParseReader, SimpleDirectoryReader
+} from "llamaindex";
+
+async function saveMarkdown(mdContent: string, filePath: string) {
+    try {
+        await fs.writeFile(filePath, mdContent, 'utf8');
+        console.log(`Markdown file has been saved to: ${filePath}`);
+    } catch (error) {
+        console.error('Error saving Markdown file:', error);
+    }
+}
+
+async function main() {
+    dotenv.config();
+
+    const dir = process.env.FILE_DIR;
+
+    const reader = new SimpleDirectoryReader();
+
+    if (dir) {
+        const documents = await reader.loadData(dir)
+
+        documents.forEach(data=>{
+            saveMarkdown(data.text, process.env.SAVE_MARKDOWN_PATH || "./output.md")
+
+        })
+    }
+}
+
+main().catch(console.error);
